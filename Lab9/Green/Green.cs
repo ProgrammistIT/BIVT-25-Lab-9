1552177c637f44016fab02df6b8463c7c2d7869e
ProgrammistIT <166043743+ProgrammistIT@users.noreply.github.com>
2026-04-18 17:23:52 +0300

    Update Green.cs

--- Lab9/Green/Green.cs
+++ Lab9/Green/Green.cs
@@ -1,3 +1,23 @@
-﻿namespace Lab9.Green
+using System;
+
+namespace Green
 {
+    public abstract class Green
+    {
+        private string _input;
+        public string Input => _input;
+
+        protected Green(string text)
+        {
+            _input = text;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+    }
 }
